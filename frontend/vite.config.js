cd8d33ba58483f4f4898e037dcdecc3bad3cfb66
Pablo Sanhueza <Sanhueza.pem@gmail.com>
2025-07-17 03:20:25 -0400

    Revert "dep"
    This reverts commit 28a67742223543441c2402d194331b1876fc8862.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://pablosanhueza1.github.io/dashboard-mercado-laboral-latam',
+  base: '/dashboard-mercado-laboral-latam/docs/',
 })
