d4f1a87cb338b0ecaa213be918a224efdc4445c0
Pablo Sanhueza <Sanhueza.pem@gmail.com>
2025-07-17 03:05:38 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "pablosanhueza1.github.io/dashboard-mercado-laboral-latam",
+  base: "https://pablosanhueza1.github.io/dashboard-mercado-laboral-latam",
 })
 // ...existing config...
